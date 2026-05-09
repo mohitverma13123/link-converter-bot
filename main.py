7c32b693154dcd2b63b15766bc40f1e535c3f13e
mohitverma13123 <mohitverma13123@gmail.com>
2026-05-09 20:31:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -559,7 +559,8 @@ async def main():
 
 if __name__ == "__main__":
 
-    asyncio.run(main())import os
+    if __name__ == "__main__":
+    asyncio.run(main())
 import re
 import logging
 import asyncio
